81e293d16edf769a34492d989e1cc75e147a95cd
aaposarlin <aapo.sarlin@hotmail.fi>
2024-10-29 20:40:38 +0200

    OrderInfo created

---
+++ src/OrderInfo.jsx
@@ -0,0 +1,25 @@
+function OrderInfo({product, quantity, totalPrice}) {
+    return (
+        <div className="order-summary">
+            <b>Order info</b>
+            <table>
+                <thead>
+                    <tr>
+                        <th><b>Prdouct</b></th>
+                        <th><b>Quantity</b></th>
+                        <th><b>Total</b></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{product}</td>
+                        <td>{quantity}</td>
+                        <td>{totalPrice}€</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default OrderInfo;
